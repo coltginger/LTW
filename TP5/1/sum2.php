98a8f3279cc6b5b13c6aa6896606e4be0990d282
coltginger <gnbarroso@gmail.com>
2024-04-21 19:27:09 +0100

    database update

---
+++ TP5/1/sum2.php
@@ -0,0 +1,11 @@
+<?php
+    $n1 = $_GET["num1"];
+    $n2 = $_GET["num2"];
+?>
+<html>
+    <head> Soma </head>
+    <body>
+        <p> <?php echo "$n1 + $n2 =" . ($n1 + $n2); ?> </p>
+        <a href="form2.html">Make another sum.</a>
+    </body>
+</html>
